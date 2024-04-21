3e9f04da9c161911595a3c6e49d740985ee16911
DragonMoffon <happydog.aja@gmail.com>
2024-04-22 03:15:37 +1200

    start shaders -.-

---
+++ pymarch/shaders/ray_march_cs.glsl
@@ -0,0 +1,9 @@
+#version 430
+
+// This is where we will be doing the ray marching eventually
+
+
+
+void main() {
+
+}
--- pymarch/shaders/ray_march_fs.glsl
+++ pymarch/shaders/ray_march_fs.glsl
@@ -1,10 +1,41 @@
 #version 330
 
+const int MAX_INTERATIONS = 100;
+const float MIN_INTERACTION_SIZE = 0.0001;
+const float MAX_TRAVEL_LENGTH = 1000.0;
+
+struct Sphere {
+    vec3 pos;
+    vec3 colour;
+    float radius;
+};
+
+uniform SceneBlock {
+    ivec4 shape_counts;
+    Sphere spheres[];
+} scene;
+
 uniform sampler2D texture0;
 
 in vec2 vs_uv;
 
 out vec4 fs_colour;
+
+
+float SDFSphere(vec3 position, vec3 origin, float radius){
+    return length(position - origin) - radius;
+}
+
+
+float get_world_dist(vec3 position){
+    return 0.0;
+}
+
+vec3 get_world_normal(vec3 position){
+    return vec3(1.0, 0.0, 0.0);
+}
+
+
 
 void main() {
     fs_colour = texture(texture0, vs_uv);
