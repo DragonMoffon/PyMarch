761ede2745c9139dce81c4bc4701e714bb905d4e
DragonMoffon <happydog.aja@gmail.com>
2024-04-22 03:38:50 +1200

    shading :0

--- pymarch/application.py
+++ pymarch/application.py
@@ -10,14 +10,15 @@ from pymarch.march import MarchScene
 class MarchWindow(Window):
 
     def __init__(self):
-        super().__init__(1280, 720, "PyMarch Window [1280, 720]")
-        a = Sphere(Vec3(300.0, 150.0, -30.0), Vec3(1.0, 0.0, 0.0), 50.0)
-        b = Sphere(Vec3(200.0, 400.0, -20.0), Vec3(0.0, 1.0, 0.0), 15.0)
-        c = Sphere(Vec3(0, 0, -50.0), Vec3(0.0, 0.0, 1.0), 250.0)
+        super().__init__(1280, 720, "PyMarch Window [1280, 720]", update_rate=1/120)
+        a = Sphere(Vec3(300.0, 150.0, -50.0), Vec3(1.0, 0.0, 0.0), 50.0)
+        b = Sphere(Vec3(200.0, 400.0, -100.0), Vec3(0.0, 1.0, 0.0), 60.0)
+        c = Sphere(Vec3(100.0, 100.0, -50.0), Vec3(0.0, 0.0, 1.0), 100.0)
         camera = CameraData()
         self.march_scene = MarchScene((a, b, c), camera)
 
     def on_update(self, delta_time: float):
+        print(1/delta_time)
         self.march_scene.do_marches(500)
 
     def on_draw(self):
--- pymarch/march.py
+++ pymarch/march.py
@@ -8,6 +8,9 @@ from pymarch.SDFs.marchstruct import MarchStruct
 from pymarch.render_target import RenderTarget
 
 
+SMALL_STEP = Vec3(0.0001, 0.0, 0.0)
+
+
 class MarchScene:
 
     def __init__(self, structs: tuple[MarchStruct, ...], camera: CameraData):
@@ -16,6 +19,8 @@ class MarchScene:
         self.max_step: int = 10
         self.intersection_dist: float = 0.0005
 
+        self.light_dir: Vec3 = Vec3(1.0, 0.5, 0.25).normalize()
+
         self.camera: CameraData = camera
 
         self.structs: tuple[MarchStruct, ...] = structs
@@ -23,24 +28,31 @@ class MarchScene:
 
         self.points = set((x, y) for x in range(self.render_target.size[0]) for y in range(self.render_target.size[1]))
 
+    def _get_world_dist(self, position: Vec3) -> float:
+        return min(self.max_dist, min(struct.SDF(position) for struct in self.structs))
+
+    def _get_closest_struct(self, position: Vec3) -> MarchStruct:
+        return self.structs[min((struct.SDF(position), idx) for idx, struct in enumerate(self.structs))[-1]]
+
+    def _get_world_normal(self, position: Vec3) -> Vec3:
+        grad_x = self._get_world_dist(position + SMALL_STEP.xyy) - self._get_world_dist(position - SMALL_STEP.xyy)
+        grad_y = self._get_world_dist(position + SMALL_STEP.yxy) - self._get_world_dist(position - SMALL_STEP.yxy)
+        grad_z = self._get_world_dist(position + SMALL_STEP.yyx) - self._get_world_dist(position - SMALL_STEP.yyx)
+
+        return Vec3(grad_x, grad_y, grad_z).normalize()
+
     def _march_step(self, position: Vec3, direction: Vec3, travel_dist: float, step_count: int):
         if travel_dist >= self.max_dist or step_count > self.max_step:
-            return 255, 255, 255
+            return 30, 30, 30
 
-        closest_dist = self.max_dist
-        closest_struct = None
-        for struct in self.structs:
-            dist = struct.SDF(position)
-            if closest_dist >= dist:
-                closest_dist = dist
-                closest_struct = struct
-
-        if closest_struct is None:
-            return 0, 0, 0
+        closest_dist = self._get_world_dist(position)
 
         if closest_dist <= self.intersection_dist:
-            r, g, b = closest_struct.colour
-            return int(255 * r), int(255 * g), int(255 * b)
+            n = self._get_world_normal(position)
+            l = max(0.0, n.dot(self.light_dir))
+            c = self._get_closest_struct(position).colour
+            r, g, b = c * l
+            return int(r * 255), int(g * 255), int(b * 255)
 
         return self._march_step(position + direction * closest_dist, direction, travel_dist+closest_dist, step_count+1)
 
